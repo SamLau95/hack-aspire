1be15a5258056393ade56f853ff79886cbea3857
Sam Lau <samlau95@gmail.com>
2014-11-02 08:42:47 -0800

    Remove performance

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,25 +1,17 @@
 class TestsController < ApplicationController
   def show
-  	@test = Test.first
-  	@questions = @test.questions
+    @test = Test.first
+    @questions = @test.questions
   end
 
   def submit
-  	answers = params[:answers]
-  	count = 0
-    correct = 0
-    answers.each do |key, val|
-  	  q = Question.find(key.to_i)
-  	  a = Answer.create! question_id: key.to_i,
-  	  		             student_id: current_user.id,
-  	  		             answer: val
-      count += 1
-      if q.correct_answer == val
-        correct += 1
-      end
-  	end
-    current_user.performance = "#{correct}/#{count}"
-    current_user.save
+    answers = params[:answers]
+    answers.each do |question_id, answer|
+      q = Question.find(question_id)
+      a = Answer.create! question_id: question_id,
+                         student: current_user,
+                         answer: answer
+    end
     redirect_to done_page_path
   end
 end
