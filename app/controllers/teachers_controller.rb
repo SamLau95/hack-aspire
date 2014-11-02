4616f1a85ce60e3e26cb9da2730a1f2ed6429e61
Alton Zheng-Xie <altonzheng@berkeley.edu>
2014-11-02 10:50:26 -0800

    d3 stuff

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,7 +3,7 @@ class TeachersController < ApplicationController
 
   def analytics
     data = DataExtractor.new(Student.taken_test).execute
-    @clustering = KMeans.new(data, data[0].length, 2).get_labels
+    #@clustering = KMeans.new(data, data[0].length, 2).get_labels
   end
 
   def students
