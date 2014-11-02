2041e44918ec2db00c2217bdfa7af6a97d4da850
Vincent Tian <vincenttian16@gmail.com>
2014-11-02 02:46:20 -0800

    tells student how many they did correctly

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,12 +7,19 @@ class TestsController < ApplicationController
   # takes the form data and processes it; stores array of 0's and 1's
   def submit
   	answers = params[:answers]
-  	answers.each do |key, val|
+  	count = 0
+    correct = 0
+    answers.each do |key, val|
   	  q = Question.find(key.to_i)
   	  a = Answer.create! question_id: key.to_i,
   	  		             student_id: current_user.id,
   	  		             answer: val
+      count += 1
+      if q.correct_answer == val
+        correct += 1
+      end
   	end
+    puts "#{correct}/#{count} scored correctly!"
     redirect_to done_page_path
   end
 end
